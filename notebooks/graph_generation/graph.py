2fa92bc8f2713ccd45ab832064993e001fa16394
Sitong An <ansitong@gmail.com>
2019-07-19 18:58:27 +0200

    Add integration; refactor make_graph to use same preprocessing code

--- notebooks/graph_generation/graph.py
+++ notebooks/graph_generation/graph.py
@@ -4,6 +4,7 @@ from scipy.sparse import csr_matrix, find
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial import cKDTree
+import tqdm
 
 Graph = namedtuple('Graph', ['X', 'Ri', 'Ro', 'y','simmatched'])
 
@@ -45,11 +46,13 @@ feature_names = ['x','y','layer','t','E']
 n_features = len(feature_names)
 
 #thanks Steve :-)
-def draw_sample(X, Ri, Ro, y, 
+def draw_sample_validation(X, Ri, Ro, y, 
                 cmap='bwr_r', 
                 skip_false_edges=True,
                 alpha_labels=False, 
-                sim_list=None): 
+                sim_list=None,
+                particular_simcluster_list=None,
+                itsn=None): 
     # Select the i/o node features for each segment    
     feats_o = X[find(Ro)[0]]
     feats_i = X[find(Ri)[0]]    
@@ -57,15 +60,7 @@ def draw_sample(X, Ri, Ro, y,
     fig, (ax0,ax1) = plt.subplots(1, 2, figsize=(20,12))
     cmap = plt.get_cmap(cmap)
     
-    if sim_list is None:    
-        # Draw the hits (layer, x, y)
-        ax0.scatter(X[:,0], X[:,2], c='k')
-        ax1.scatter(X[:,1], X[:,2], c='k')
-    else:        
-        #ax0.scatter(X[:,0], X[:,2], c='k')
-        #ax1.scatter(X[:,1], X[:,2], c='k')
-        ax0.scatter(X[sim_list,0], X[sim_list,2], c='b')
-        ax1.scatter(X[sim_list,1], X[sim_list,2], c='b')
+
     
     # Draw the segments
     for j in range(y.shape[0]):
@@ -78,12 +73,32 @@ def draw_sample(X, Ri, Ro, y,
                  [feats_o[j,2], feats_i[j,2]], '-', **seg_args)
         ax1.plot([feats_o[j,1], feats_i[j,1]],
                  [feats_o[j,2], feats_i[j,2]], '-', **seg_args)
+        
+    if sim_list is None:    
+        # Draw the hits (layer, x, y)
+        ax0.scatter(X[:,0], X[:,2], c='k')
+        ax1.scatter(X[:,1], X[:,2], c='k')
+    else:        
+        #ax0.scatter(X[:,0], X[:,2], c='k')
+        #ax1.scatter(X[:,1], X[:,2], c='k')
+        ax0.scatter(X[sim_list,0], X[sim_list,2], c='g')
+        ax1.scatter(X[sim_list,1], X[sim_list,2], c='g')
+        
+        if particular_simcluster_list is not None:
+            ax0.scatter(X[particular_simcluster_list,0], X[particular_simcluster_list,2], c='r')
+            ax1.scatter(X[particular_simcluster_list,1], X[particular_simcluster_list,2], c='r')  
+
+        if itsn is not None:
+            ax0.scatter(X[itsn,0], X[itsn,2], c='k', marker='^')
+            ax1.scatter(X[itsn,1], X[itsn,2], c='k', marker='^')         
+        
     # Adjust axes
     ax0.set_xlabel('$x$ [cm]')
     ax1.set_xlabel('$y$ [cm]')
     ax0.set_ylabel('$layer$ [arb]')
     ax1.set_ylabel('$layer$ [arb]')
     plt.tight_layout()
+    return fig, ax0, ax1
 
 def draw_sample3d(X, Ri, Ro, y, 
                   cmap='bwr_r', 
@@ -119,27 +134,3 @@ def draw_sample3d(X, Ri, Ro, y,
     ax2.set_ylabel('$y$ [cm]')
     ax2.set_zlabel('$layer$ [arb]')
     
-def make_graph_kdtree(coords,layers,sim_indices,r=2.5):
-    #setup kd tree for fast processing
-    the_tree = cKDTree(coords)
-    
-    #define the pre-processing (all layer-adjacent hits in ball R < r)
-    #and build a sparse matrix representation, then blow it up 
-    #to the full R_in / R_out definiton
-    pairs = the_tree.query_pairs(r=r,output_type='ndarray')
-    first,second = pairs[:,0],pairs[:,1]  
-    #selected index pair list that we label as connected
-    pairs_sel  = pairs[( (np.abs(layers[(second,)]-layers[(first,)]) <= 1)  )]
-    data_sel = np.ones(pairs_sel.shape[0])
-    
-    #prepare the input and output matrices (already need to store sparse)
-    r_shape = (coords.shape[0],pairs.shape[0])
-    eye_edges = np.arange(pairs_sel.shape[0])
-    
-    R_i = csr_matrix((data_sel,(pairs_sel[:,1],eye_edges)),r_shape,dtype=np.uint8)
-    R_o = csr_matrix((data_sel,(pairs_sel[:,0],eye_edges)),r_shape,dtype=np.uint8)
-        
-    #now make truth graph y (i.e. both hits are sim-matched)    
-    y = (np.isin(pairs_sel,sim_indices).astype(np.int8).sum(axis=-1) == 2)
-    
-    return R_i,R_o,y
---
+++ notebooks/graph_generation/preprocessing.py
@@ -0,0 +1,103 @@
+import numpy as np
+import awkward
+from graph import Graph
+from scipy.spatial import cKDTree
+from scipy.spatial import Delaunay
+from scipy.sparse import csr_matrix, find
+from sklearn.neighbors import NearestNeighbors
+from itertools import tee
+
+def make_graph_kdtree(coords,layers,sim_indices,r):
+    #setup kd tree for fast processing
+    the_tree = cKDTree(coords)
+    
+    #define the pre-processing (all layer-adjacent hits in ball R < r)
+    #and build a sparse matrix representation, then blow it up 
+    #to the full R_in / R_out definiton
+    pairs = the_tree.query_pairs(r=r,output_type='ndarray')
+    first,second = pairs[:,0],pairs[:,1]  
+    #selected index pair list that we label as connected
+    #pairs_sel  = pairs[( (np.abs(layers[(second,)]-layers[(first,)]) <= 1)  )]
+    neighbour_counts = np.unique(pairs[:,0], return_counts=True)[1]
+    neighbour_counts = np.repeat(neighbour_counts, neighbour_counts)
+    pairs_sel  = pairs[(np.abs(layers[(second,)]-layers[(first,)]) <= 1) | (neighbour_counts == 1)]
+    #pairs_sel  = pairs
+    data_sel = np.ones(pairs_sel.shape[0])
+    
+    #prepare the input and output matrices (already need to store sparse)
+    r_shape = (coords.shape[0],pairs.shape[0])
+    eye_edges = np.arange(pairs_sel.shape[0])
+    
+    R_i = csr_matrix((data_sel,(pairs_sel[:,1],eye_edges)),r_shape,dtype=np.uint8)
+    R_o = csr_matrix((data_sel,(pairs_sel[:,0],eye_edges)),r_shape,dtype=np.uint8)
+        
+    #now make truth graph y (i.e. both hits are sim-matched)    
+    y = (np.isin(pairs_sel,sim_indices).astype(np.int8).sum(axis=-1) == 2)
+    
+    return R_i,R_o,y
+
+def make_graph_knn(coords, layers, sim_indices, k):
+    
+    nbrs = NearestNeighbors(algorithm='kd_tree').fit(coords)
+    pairs = np.array(nbrs.kneighbors_graph(coords, 5).nonzero()).T
+    first,second = pairs[:,0],pairs[:,1]  
+    #selected index pair list that we label as connected
+    pairs_sel  = pairs[(first != second)]
+    data_sel = np.ones(pairs_sel.shape[0])
+    
+    #prepare the input and output matrices (already need to store sparse)
+    r_shape = (coords.shape[0],pairs.shape[0])
+    eye_edges = np.arange(pairs_sel.shape[0])
+    
+    R_i = csr_matrix((data_sel,(pairs_sel[:,1],eye_edges)),r_shape,dtype=np.uint8)
+    R_o = csr_matrix((data_sel,(pairs_sel[:,0],eye_edges)),r_shape,dtype=np.uint8)
+        
+    #now make truth graph y (i.e. both hits are sim-matched)    
+    y = (np.isin(pairs_sel,sim_indices).astype(np.int8).sum(axis=-1) == 2)
+    return R_i,R_o,y    
+        
+
+
+def make_graph_xy(arrays, valid_sim_indices, ievt, mask, r, algo=make_graph_knn):
+   
+    x = arrays[b'rechit_x'][ievt][mask]
+    y = arrays[b'rechit_y'][ievt][mask]
+    z = arrays[b'rechit_z'][ievt][mask]
+    layer = arrays[b'rechit_layer'][ievt][mask]
+    time = arrays[b'rechit_time'][ievt][mask]
+    energy = arrays[b'rechit_energy'][ievt][mask]    
+    feats = np.stack((x,y,layer,time,energy)).T
+
+
+    all_sim_hits = np.unique(valid_sim_indices[ievt].flatten())
+    sim_hits_mask = np.zeros(arrays[b'rechit_z'][ievt].size, dtype=np.bool)
+    sim_hits_mask[all_sim_hits] = True
+    simmatched = np.where(sim_hits_mask[mask])[0]
+    
+    Ri, Ro, y_label = algo(np.stack((x,y,layer)).T, layer, simmatched, r=r)
+    
+    return Graph(feats, Ri, Ro, y_label, simmatched)
+
+def make_graph_etaphi(arrays, valid_sim_indices, ievt, mask, r, layered_norm, algo=make_graph_knn):
+   
+    x = arrays[b'rechit_x'][ievt][mask]
+    y = arrays[b'rechit_y'][ievt][mask]
+    z = arrays[b'rechit_z'][ievt][mask]
+    layer = arrays[b'rechit_layer'][ievt][mask]
+    time = arrays[b'rechit_time'][ievt][mask]
+    energy = arrays[b'rechit_energy'][ievt][mask]    
+    feats = np.stack((x,y,layer,time,energy)).T
+
+    eta = arrays[b'rechit_eta'][ievt][mask]
+    phi = arrays[b'rechit_phi'][ievt][mask]
+    layer_normed = layer / layered_norm
+    
+    all_sim_hits = np.unique(valid_sim_indices[ievt].flatten())
+    sim_hits_mask = np.zeros(arrays[b'rechit_z'][ievt].size, dtype=np.bool)
+    sim_hits_mask[all_sim_hits] = True
+    simmatched = np.where(sim_hits_mask[mask])[0]
+    
+    #Ri, Ro, y_label = make_graph_kdtree(np.stack((eta, phi, layer_normed)).T, layer, simmatched, r=r)
+    Ri, Ro, y_label = algo(np.stack((eta, phi, layer_normed)).T, layer, simmatched, k=3)
+    
+    return Graph(feats, Ri, Ro, y_label, simmatched)
